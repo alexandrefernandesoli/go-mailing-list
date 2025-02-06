4d840d708eac3c11191e756dcf41062eca318fb8
Alexandre Fernandes de Oliveira <alexandrefernandes_1@hotmail.com>
2025-02-06 01:15:45 -0400

    fix: upx

--- tiny-bundle.mk
+++ tiny-bundle.mk
@@ -9,5 +9,4 @@ build:
 	@npm run build:tailwind
 	@templ generate
 	@go build -tags $(TAGS) -ldflags $(LDFLAGS) -o bin/app_prod cmd/app/main.go
-	@upx bin/app_prod
 	@echo "compiled you application with all its assets to a single binary => bin/app_prod"
